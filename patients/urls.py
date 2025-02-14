b7ae1166fccf679b7d29e020dd3e5fb66ceb9487
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-02-14 22:48:54 +0100

    Creat a view for each consult and do him pubic for patient if have payment ok!

--- patients/urls.py
+++ patients/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.patients, name='patients'),
     path('<int:id>', views.patient_view, name='patient_view'),
     path('upgrade/<int:id>', views.upgrade_patient, name='upgrade_patient'),
+    path('delete_consult/<int:id>', views.delete_consult, name='delete_consult'),
+    path('public_consult/<int:id>', views.public_consult, name='public_consult'),
 ]
--- patients/views.py
+++ patients/views.py
@@ -1,5 +1,6 @@
+from django.http import Http404
 from django.shortcuts import render, redirect
-from .models import Patients
+from .models import Patients, Todos, Consult
 from django.contrib import messages
 from django.contrib.messages import constants
 
@@ -33,7 +34,31 @@ def patients(request):
 def patient_view(request, id):
     patient = Patients.objects.get(id=id)
     if request.method == 'GET':
-        return render(request, 'patient.html', {'patient': patient})
+        todos = Todos.objects.all()
+        consults = Consult.objects.filter(patient=patient)
+        return render(request, 'patient.html', {'patient': patient, 'todos': todos, 'consults': consults})
+    else:
+        humor = request.POST.get('humor')
+        geral_register = request.POST.get('geral_register')
+        video = request.FILES.get('video')
+        todos = request.POST.getlist('todos')
+
+        consults = Consult(
+            humor=int(humor),
+            geral_register=geral_register,
+            video=video,
+            patient=patient
+        )
+        consults.save()
+
+        for t in todos:
+            todo = Todos.objects.get(id=t)
+            consults.todo.add(todo)
+
+        consults.save()
+         
+        messages.add_message(request, constants.SUCCESS, 'Registro de consulta adicionado com sucesso.')
+        return redirect(f'/patients/{id}')
 
 def upgrade_patient(request, id):
     payments_status = request.POST.get('payments_status')
@@ -43,3 +68,15 @@ def upgrade_patient(request, id):
     patient.save()
 
     return redirect(f'/patients/{id}')
+
+def delete_consult(request, id):
+    consult = Consult.objects.get(id=id)
+    consult.delete()
+    return redirect(f'/patients/{consult.patient.id}')
+
+def public_consult(request, id):
+    consults = Consult.objects.get(id=id)
+    print(consults.link_publico)
+    if not consults.patient.payments_status:
+        raise Http404()
+    return render(request, 'public_consult.html', {'consults': consults})
